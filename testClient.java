2695c4b996d6052299cd813db76fb8a40a505d6a
Yugiobattu420 <nhatduy0409@gmail.com>
2023-01-02 10:11:38 +0700

    Update

--- testClient.java
+++ testClient.java
@@ -1,43 +1,114 @@
 import java.io.*;
+import java.util.*;
 import java.net.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.ArrayList;
 
-public class testClient 
+public class testClient implements Serializable
 {
-	public static void main(String arg[])
+	public static Boolean flag = false;
+
+	public static void main(String arg[]) throws IOException
 	{
 		try
 		{
+			folder f = new folder("Folder 1");
+			System.out.println(f);
 			Socket s = new Socket("localhost",3200);
 			System.out.println(s.getPort());
 
 			System.out.println("Talking to Server");
 
+			folder[] listF = new folder[3];
+			listF[0] = new folder("Folder 1");
+			listF[1] = new folder("Folder 2");
+			listF[2] = new folder("Folder 3");
+
+
 			Thread outThread = new Thread() {
 				@Override
 				public void run() {
 					try {
-						OutputStream os=s.getOutputStream();
-						BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os));
+						OutputStream outputStream = s.getOutputStream();
+						ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
 
-						String sentMessage="";
-
-						while (true) {
-							DataInputStream din=new DataInputStream(System.in);
-							sentMessage=din.readLine();
-							bw.write(sentMessage);
-							bw.newLine();
-							bw.flush();
-
-							if (sentMessage.equalsIgnoreCase("quit"))
-								break;
-							
+						if (flag == false){
+							List<Message> messages = new ArrayList<>();
+							messages.add(new Message("list"));
+							messages.add(new Message(Integer.toString(s.getPort())));
+							for (int i = 0; i < listF.length; i++){
+								messages.add(new Message(listF[i].getName()));
+							}
+							objectOutputStream.writeObject(messages);
+							flag = true;
 						}
-						bw.close();
+
+						while(true){
+							Scanner sc = new Scanner(System.in);
+
+							String fd;
+							System.out.println();
+							System.out.println("Client has three folder: 1: Folder 1, 2: Folder 2, 3: Folder 3");
+							System.out.print("Chose folder you want to do something with it: ");
+							fd = sc.nextLine();
+
+							String choice;
+							System.out.println();
+							System.out.println("0. Exit");
+							System.out.println("1. Edit name folder");
+							System.out.println("2. Add file");
+							System.out.println("3. Delete file");
+							System.out.print("Chose action you want to do with this file: ");
+							choice = sc.nextLine();
+
+							int index = Integer.parseInt(fd) - 1;
+
+							if (choice.equals("1")){
+
+								System.out.println();
+								System.out.print("Enter new name of folder: ");
+								String name = sc.nextLine();
+								listF[index].editName(name);
+
+								List<Message> messages = new ArrayList<>();
+								messages.add(new Message(listF[index].getName()));
+								messages.add(new Message(listF[index].getAction()));
+
+								objectOutputStream.writeObject(messages);
+								
+							}
+							else if (choice.equals("2")){
+								listF[index].addFile();
+
+								List<Message> messages = new ArrayList<>();
+								messages.add(new Message(listF[index].getName()));
+								messages.add(new Message(listF[index].getAction()));
+
+								objectOutputStream.writeObject(messages);
+							}
+							else if (choice.equals("3")){
+								listF[index].deleteFile();
+
+								List<Message> messages = new ArrayList<>();
+								messages.add(new Message(listF[index].getName()));
+								messages.add(new Message(listF[index].getAction()));
+
+								objectOutputStream.writeObject(messages);
+							}
+							else if (choice.equals("0")){
+								List<Message> messages = new ArrayList<>();
+								messages.add(new Message("Exit"));
+								break;
+							}
+						}
+						outputStream.close();
+						objectOutputStream.close();
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
-					} finally {
-					}
+					} 
 				};
 			};
 			outThread.start();
@@ -66,5 +137,39 @@ public class testClient
 		{
 			System.out.println("There're some error");
 		}
+	}
+}
+
+class folder{
+	String name;
+	String action;
+
+	public folder(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return this.name;
+	}
+
+	public String getAction(){
+		return this.action;
+	}
+
+	public void editName(String name){
+		this.name = name;
+		this.action = "Edit Name Folder";
+	}
+
+	public void addFile(){
+		this.action = "Add file";
+	}
+
+	public void deleteFile(){
+		this.action = "Delete file";
+	}
+
+	public String toString(){
+		return this.name;
 	}
 }
--- testServer.java
+++ testServer.java
@@ -1,67 +1,186 @@
 import java.io.*;
 import java.net.*;
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.net.*;
+import javax.swing.table.DefaultTableModel;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
-public class testServer 
+public class testServer extends JFrame
 {
-	public static void main(String arg[])
+	private final String[] columns = {"Client", "Name Folder", "Change"};
+	private final DefaultTableModel defaultTableModel = new DefaultTableModel(columns, 0);
+	Map<String, String> mapPort = new HashMap<String, String>();
+	Map<String, String> map = new HashMap<String, String>();
+	int hostNumber = 0;
+	JLabel[] label = {new JLabel("Host"), new JLabel("Host"), new JLabel("Host"), new JLabel("Host")};
+	DefaultComboBoxModel[] host = {new DefaultComboBoxModel(), new DefaultComboBoxModel(), new DefaultComboBoxModel(), new DefaultComboBoxModel()};
+
+
+	// public void addHost(String host, String[] folder){
+
+	// 	JPanel panel = new JPanel();
+	// 	topPanel.setLayout(new GridLayout(1,2));
+
+	// 	JPanel nameHost = new JPanel();
+	// 	JLabel name = new JLabel("Host");
+	// 	nameHost.add(name);
+
+	// 	JPanel chose = new JPanel();
+	// 	for (int i = 0; i < folder.length; i++){
+	// 		System.out.println(folder[i]);
+	// 	}
+	// 	JComboBox petList = new JComboBox(folder);
+	// 	chose.add(petList);
+
+	// 	panel.add(nameHost);
+	// 	panel.add(chose);
+
+	// 	topPanel.add(panel);
+	// }
+
+	public static void main(String arg[]){
+		new testServer();
+	}
+	
+	public testServer()
 	{
+
 		try
 		{
+			setTitle("Folder Watcher Socket");
+			setSize(800, 600);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+			JPanel topPanel = new JPanel();
+			topPanel.setLayout(new GridLayout(1,2));
+			for (int i = 0; i < 4; i++){
+				JPanel panel = new JPanel();
+
+				JPanel nameHost = new JPanel();
+				JLabel name = label[i];
+				nameHost.add(name);
+
+				JPanel chose = new JPanel();
+				
+				JComboBox h = new JComboBox(host[i]);
+				h.setActionCommand(Integer.toString(i));
+				h.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						JComboBox cb = (JComboBox)e.getSource();
+        				String folder = (String)cb.getSelectedItem();
+						String command = e.getActionCommand();
+						map.put(command, folder);
+					}
+				});
+				chose.add(h);
+
+				panel.add(nameHost);
+				panel.add(chose);
+				topPanel.add(panel);
+			};
+
+			JTable table = new JTable(defaultTableModel);
+			table.setEnabled(false);
+
+			JPanel centerPanel = new JPanel();
+			centerPanel.setLayout(new BorderLayout());
+			centerPanel.add(new JScrollPane(table));
+
+			add(centerPanel, BorderLayout.CENTER);	
+
+			add(topPanel, BorderLayout.NORTH);
+
+			setVisible(true);		
+
 			ServerSocket s = new ServerSocket(3200);
 
 			System.out.println("Waiting for a Client");
 			
-			Socket ss=s.accept();
-			System.out.println("Talking to client");
-			System.out.println(ss.getPort());
+			while(true){
 
-			Thread outThread = new Thread() {
-				@Override
-				public void run() {
-					try {
-						OutputStream os=ss.getOutputStream();
-						BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os));
+				Socket ss;
+				if (hostNumber < 4){
+					ss=s.accept();
+					mapPort.put(Integer.toString(ss.getPort()), Integer.toString(hostNumber));
+					hostNumber++;
+					System.out.println(hostNumber);
+				}
+				else {
+					break;
+				}
+				// System.out.println("Talking to client");
+				// System.out.println(ss.getPort());
 
-						while (true) {
-							DataInputStream din=new DataInputStream(System.in);
-							String k=din.readLine();
-							bw.write(k);
-							bw.newLine();
-							bw.flush();
-						}
-						//bw.close();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} 
-				};
-			};
-			outThread.start();
+				Thread inThread = new Thread() {
+					@Override
+					public void run() {
+						try {
+							InputStream inputStream = ss.getInputStream();
+							ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);		
+							
+							while(true){
+								List<Message> listOfMessages = (List<Message>) objectInputStream.readObject();
+								if (listOfMessages.get(0).getText().equals("list")){
+									List<String> folder = new ArrayList<String>();
+									listOfMessages.forEach((msg)-> folder.add(msg.getText()));
 
-			Thread inThread = new Thread() {
-				@Override
-				public void run() {
-					try {
-						InputStream is=ss.getInputStream();
-						BufferedReader br=new BufferedReader(new InputStreamReader(is));
-						while (true) {
-							String receivedMessage;
+									String[] ls = folder.toArray(new String[0]);
+									label[hostNumber - 1].setText(Integer.toString(ss.getPort()));
+									for (int i = 2; i < ls.length; i++){
+										host[hostNumber - 1].addElement(ls[i]);
+									}
+								}
+								else {
+									System.out.println("Received [" + listOfMessages.size() + "] messages from: " + ss);
+									System.out.println("All messages:");
+									listOfMessages.forEach((msg)-> System.out.println(msg.getText()));
 
-							receivedMessage=br.readLine();
-							System.out.println("Received : " + receivedMessage);
-							if (receivedMessage.equalsIgnoreCase("quit"))
-							{
-								System.out.println("Client has left !");
-								break;
+									if (listOfMessages.get(0).equals("Exit")){
+										break;
+									}
+									System.out.println("Test1");
+									for (Map.Entry<String, String> entry : mapPort.entrySet()) {
+										System.out.println(entry.getKey() + " " + entry.getValue());
+									};
+									System.out.println("Test2");
+									for (Map.Entry<String, String> entry : map.entrySet()) {
+										System.out.println(entry.getKey() + " " + entry.getValue());
+									};
+									String id = mapPort.get(Integer.toString(ss.getPort()));
+									String folder = map.get(id);
+									// System.out.println(id);
+									// System.out.println(folder);
+									// System.out.println(listOfMessages.get(0).getText());
+									if (listOfMessages.get(0).getText().equals(folder)){
+										defaultTableModel.insertRow(0, new Object[]{ss.getPort(), listOfMessages.get(0).getText(), listOfMessages.get(1).getText()});
+									}
+									
+								}
 							}
-						}
-						br.close();
-					} catch (Exception e) {
-	//					e.printStackTrace();
-					} 
+
+							inputStream.close();
+							objectInputStream.close();
+
+							ss.close();
+						} catch (Exception e) {
+		//					e.printStackTrace();
+						} 
+					};
 				};
-			};
-			inThread.start();
+				inThread.start();
+			}
 			
 		}
 		catch(IOException e)
